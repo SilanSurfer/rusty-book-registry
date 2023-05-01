392811bfa937ac5be4ae0fe1f5886a163b73756e
SilanSurfer <szwarc.adam@gmail.com>
2023-05-01 22:10:56 +0200

    [feat] Added reading of the app config from configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use rusty_book_registry::configration::get_configuration;
 use rusty_book_registry::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
-    let address = listener.local_addr()?;
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = std::net::TcpListener::bind(address.clone())?;
     println!("Server started for {}", address);
     run(listener)?.await
 }
