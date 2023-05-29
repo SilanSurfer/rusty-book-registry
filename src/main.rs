ecd89232f3b51bc861910ec04fcdc0253fea4983
SilanSurfer <szwarc.adam@gmail.com>
2023-05-29 21:40:40 +0200

    [feat] Added logging middleware using log crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use env_logger::Env;
 use rusty_book_registry::configration::get_configuration;
 use rusty_book_registry::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
