147451ebeca97d6bc3493f4404fc79054469cdef
SilanSurfer <szwarc.adam@gmail.com>
2023-05-30 22:36:50 +0200

    [feat] Added tracing subscriber and formatter

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use env_logger::Env;
 use rusty_book_registry::configration::get_configuration;
 use rusty_book_registry::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Redirect log events to tracing subscriber
+    LogTracer::init().expect("Failed to set logger");
+    // Configuration of tracing
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer =
+        BunyanFormattingLayer::new("rusty_book_registry".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
