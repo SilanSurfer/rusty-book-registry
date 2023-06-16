4d7919ec3bb0fc23212b8a2ce8e68c2d4634016d
SilanSurfer <szwarc.adam@gmail.com>
2023-06-16 22:35:04 +0200

    [test] Added property-based tests for email validation
    Testcase for parsing valid email is now done using property-based
    testing using crates quickcheck and fake

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,7 +22,9 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use super::SubscriberEmail;
-    use claim::{assert_err, assert_ok};
+    use claim::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
 
     #[test]
     fn empty_string_should_fail() {
@@ -42,9 +44,20 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn email_with_correct_format_should_be_parsed_succesfully() {
-        let email = "BruceWayne@domain.com".to_string();
-        assert_ok!(SubscriberEmail::parse(email));
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn email_with_correct_format_should_be_parsed_succesfully(
+        valid_email: ValidEmailFixture,
+    ) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
